c79be0fd85e8354a36e6b4677873d56c2a950784
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-02 10:43:30 +0530

    HelloWorld.sol added

---
+++ HelloWorld.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract helloWorld{
+function fun() public pure returns (bool, uint256, bytes32) {
+        bool x;
+        uint256 y;
+        bytes32 z;
+
+        assembly {
+            x := 1 // true 
+            y := 0xa  // 10
+            z := "Hello World!" // "0x48656c6c6f20576f726c64210000000000000000000000000000000000000000"
+        }
+
+        return( x, y, z);
+    }
+}
