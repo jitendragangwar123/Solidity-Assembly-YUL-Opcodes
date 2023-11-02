2bff5a391f00a49a5dff1482222bfd46f3875cb4
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-02 11:12:56 +0530

    DataTypes.sol

--- Yul_DataTypes.sol
+++ Yul_DataTypes.sol
@@ -1,7 +1,6 @@
-```
+/*
 Use assembly keyword for writing the smart contract in Yul.
-```
-```shell
+Example:
 contract C {
     function f() public {
         assembly {
@@ -9,12 +8,24 @@ contract C {
         }
     }
 }
-```
+*/
 
+/*
+Variable :- To declare a variable with let keyword.
+Example:
+let x := 1
+*/
+
+/*
+Yul Types :- Yul has only 1 type: bytes32. This can hold any value. The compiler will automatically insert conversions as needed.
+Integer : constants in decimal or hexadecimal notation.
+ASCII strings : Which may contain hex escapes \xNN and Unicode escapes \uNNNN where N are hexadecimal digits.
+Hex strings : (e.g. hex"616263").
+*/
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-contract helloWorld{
+contract DataTypes{
 function fun() public pure returns (bool, uint256, bytes32) {
         bool x;
         uint256 y;
