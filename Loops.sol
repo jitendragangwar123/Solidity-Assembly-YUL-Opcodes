576de229e08c700345a9a9e06e8c7014b2b3c93b
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-02 12:13:52 +0530

    Feat: break statement in for loop

--- Loops.sol
+++ Loops.sol
@@ -34,5 +34,25 @@ function forLoop(uint256 n) public pure returns(uint256){
     }
 }
 
+/*
+break Statement :
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract Loops{
+function forLoop(uint256 n) public pure returns(uint256){
+        uint256 sum=0;
+        assembly {
+            for {let i := 0} lt(i, n) {i := add(i, 1)} {
+                sum:= add(sum,i)
+                if eq(i,3){
+                    break
+                }
+            }
+        }
+        return sum;
+    }
+}
 
 
